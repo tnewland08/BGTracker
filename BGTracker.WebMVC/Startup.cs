57e84bb709237b08557cf329ed180c0ac4bd9aa9
Todd Newland <tnewland08@gmail.com>
2020-06-27 15:23:18 -0400

    Add project files.

---
+++ BGTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BGTracker.WebMVC.Startup))]
+namespace BGTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
