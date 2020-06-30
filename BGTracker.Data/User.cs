b07c50377685d21314b3045432a6d08418762e42
Todd Newland <tnewland08@gmail.com>
2020-06-29 20:32:25 -0400

    added User table

---
+++ BGTracker.Data/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGTracker.Data
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name ="Type 1")]
+        public bool TypeOne { get; set; }
+
+        [Required]
+        [Display(Name ="Type 2")]
+        public bool TypeTwo { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
